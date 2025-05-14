410f3bb053167ade5da8684a4bf99fcb15945b54
Anonimys-cyber <qwerty.ytrewq.lkjhg@gmail.com>
2025-05-14 16:44:01 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
-$host = "sql213.ezyro.com";
+$host = getenv("DB_HOST");
 $port = 3306;
-$dbname = "ezyro_38975348_unity_game";
-$user = "ezyro_38975348";
-$password = "4247c00a4d1f";
+$dbname = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
 
-$conn = new mysqli($host, $user, $password, $dbname, $port);
+$conn = new mysqli($host, $user, $password, $dbname);
 
 if ($conn->connect_error) {
     die(json_encode(["error" => "Connection to the database failed: " . $conn->connect_error]));

97d9512eccab8e18db36d3a24086b5df2db1873d
Anonimys-cyber <qwerty.ytrewq.lkjhg@gmail.com>
2025-05-22 14:32:31 +0300

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $dbname = getenv("DB_NAME");
 $user = getenv("DB_USER");
 $password = getenv("DB_PASSWORD");
 
-$conn = new mysqli($host, $user, $password, $dbname);
+$conn = new mysqli($host, $user, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die(json_encode(["error" => "Connection to the database failed: " . $conn->connect_error]));
